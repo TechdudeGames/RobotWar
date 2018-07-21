22cddc4694cd7acb786367aeb6d69e3c7649c3d2
TechdudeGames <TechdudeGames@users.noreply.github.com>
2018-07-21 14:50:49 -0600

    Fixed Bugs & Comments
    Shrunk a bug where if a thread modified the real-time statistic list after a totalup() was called it the print statements would show inaccurate data.
    
    Fixed a bug where the terminal would clobber some of the ending results with the command line.
    
    Added comments about how the code works so I don't have to scratch my head later and wonder how the heck the code works.

--- MultiWar.py
+++ MultiWar.py
@@ -16,7 +16,7 @@ print("Playing %i games." %(number_of_total_games//avcores * avcores))
 
 rtstatlist = []
 for loops in range(0, avcores):
-    stat = multiprocessing.Array('i', range(4))
+    stat = multiprocessing.Array('i', range(4)) #creating a statistic list for a thread to utalize
     for kount in range(0,4):
         stat[kount] = 0
     rtstatlist.append(stat)
@@ -36,10 +36,20 @@ def warthread(numgames,threadnum,statlist):
         statlist[threadnum][3] += 1
 
 def totalup(statlist,dataindex,numbcores):
+    """
+    :param statlist: The collective thread statistics lists
+    :param dataindex: The data from each list to add up
+    0-Player one wins
+    1-Player two wins
+    2-Draws
+    3-Total games played
+    :param numbcores: The number of threads (Used for a for statement to correctly add up the data)
+    :return: The total value of each threads statistics combines
+    """
     returnresult = 0
     internalcount = numbcores
     for dive in range(0,numbcores):
-        returnresult += statlist[dive][dataindex]
+        returnresult += statlist[dive][dataindex] #Dive is used as an index into the realtime list of lists.
     return returnresult
 
 
@@ -58,15 +68,17 @@ else:
 #Main Event
 last_run = False
 while totalup(rtstatlist,3,avcores) != number_of_total_games//avcores * avcores:
-    if totalup(rtstatlist,3,avcores) != 0:
+    statlist=[totalup(rtstatlist,0,avcores),totalup(rtstatlist,1,avcores),totalup(rtstatlist,2,avcores),totalup(rtstatlist,3,avcores)]
+    #Prevents a bug from occuring if a thread modified the rtstatlist before the print code finshed processing the first totalup
+    if totalup(rtstatlist,3,avcores) != 0: #Prevents divide by zero error if the display code was run before any of the threads had a chance to play a game
         with term.location(0,10):
-            print("Player One has won %f percent of the time       " %float(totalup(rtstatlist,0,avcores) * 100 / totalup(rtstatlist,3,avcores)))
-            print("Player Two has won %f percent of the time       " %float(totalup(rtstatlist,1,avcores) * 100 / totalup(rtstatlist,3,avcores)))
-            print("There has been a draw %f percent of the time     \n" %float(totalup(rtstatlist,2,avcores) / totalup(rtstatlist,3,avcores)))
-            print("Player One has won %i times" %totalup(rtstatlist,0,avcores))
-            print("Player Two has won %i times" %totalup(rtstatlist,1,avcores))
-            print("There have been %i draws" %totalup(rtstatlist,2,avcores))
-            print("The game has been played %i times" %totalup(rtstatlist,3,avcores))
+            print("Player One has won %f percent of the time       " %float(statlist[0] * 100 / statlist[3]))
+            print("Player Two has won %f percent of the time       " %float(statlist[1] * 100 / statlist[3]))
+            print("There has been a draw %f percent of the time     \n" %float(statlist[2] / statlist[3]))
+            print("Player One has won %i times" %statlist[0])
+            print("Player Two has won %i times" %statlist[1])
+            print("There have been %i draws" %statlist[2])
+            print("The game has been played %i times" %statlist[3])
             elapsted_seconds = time.time()-starttime
             #elapsted_seconds = 602263 #Debug time amount. Should be 6 days, 23 hours, 17 minutes, and 43 seconds
             days = int(elapsted_seconds // 86400)
@@ -76,19 +88,18 @@ while totalup(rtstatlist,3,avcores) != number_of_total_games//avcores * avcores:
             print("Time Elapsed: ", days, ":", hours, ":", minutes, ":", seconds)
 os.system("clear")
 with term.location(0, 10):
-    print("Player One has won %f percent of the time       " % float(
-        totalup(rtstatlist, 0, avcores) * 100 / totalup(rtstatlist, 3, avcores)))
-    print("Player Two has won %f percent of the time       " % float(
-        totalup(rtstatlist, 1, avcores) * 100 / totalup(rtstatlist, 3, avcores)))
-    print("There has been a draw %f percent of the time     \n" % float(
-        totalup(rtstatlist, 2, avcores) / totalup(rtstatlist, 3, avcores)))
-    print("Player One has won %i times" % totalup(rtstatlist, 0, avcores))
-    print("Player Two has won %i times" % totalup(rtstatlist, 1, avcores))
-    print("There have been %i draws" % totalup(rtstatlist, 2, avcores))
-    print("The game has been played %i times" % totalup(rtstatlist, 3, avcores))
+    print("Player One has won %f percent of the time       " % float(statlist[0] * 100 / statlist[3]))
+    print("Player Two has won %f percent of the time       " % float(statlist[1] * 100 / statlist[3]))
+    print("There has been a draw %f percent of the time     \n" % float(statlist[2] / statlist[3]))
+    print("Player One has won %i times" % statlist[0])
+    print("Player Two has won %i times" % statlist[1])
+    print("There have been %i draws" % statlist[2])
+    print("The game has been played %i times" % statlist[3])
     elapsted_seconds = time.time() - starttime
-    days = int(elapsted_seconds//86400)
-    hours = int(elapsted_seconds//3600 - (days * 24))
-    minutes = int(elapsted_seconds//60 - (hours * 60) - (days  * 1440))
-    seconds = int(elapsted_seconds - (minutes * 60) - (hours * 3600) - (days * 86400) )
-    print("Time Elapsed: ", days, " : ", hours, " : ", minutes, " : ", seconds)
+    # elapsted_seconds = 602263 #Debug time amount. Should be 6 days, 23 hours, 17 minutes, and 43 seconds
+    days = int(elapsted_seconds // 86400)
+    hours = int(elapsted_seconds // 3600 - (days * 24))
+    minutes = int(elapsted_seconds // 60 - (hours * 60) - (days * 1440))
+    seconds = int(elapsted_seconds - (minutes * 60) - (hours * 3600) - (days * 86400))
+    print("Time Elapsed: ", days, ":", hours, ":", minutes, ":", seconds)
+term.location(0,9) #Prevents the cursior of the terminal from clobbering the the information printed above
